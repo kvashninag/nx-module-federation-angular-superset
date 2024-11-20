d1848073b5effc8b711a344bb36064d18c318a3b
aleksandr.kvashnin <aleksandr.kvashnin@apm-consult.com>
2024-11-20 12:02:43 +0500

    Init shop

--- apps/host/src/app/app.routes.ts
+++ apps/host/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'shop',
+    loadChildren: () => import('shop/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
